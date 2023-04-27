03b457e6d0e9fcdc03b9b6ad2d5a1acc259a254c
Joseph <yosephassefa85@gmail.com>
2023-04-27 17:49:05 +0300

    adding region based filter done

--- src/app/services/region/regions.service.ts
+++ src/app/services/region/regions.service.ts
@@ -1,7 +1,7 @@
 import { BaseService } from '../base-service/BaseService';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { RegionBasedSummaryData, SummaryCount } from 'src/app/models/get/SummaryCount';
 import { SummaryData } from 'src/app/models/get/SummaryData';
 import { RegionMaster } from 'src/app/models/post/region-master-model';
@@ -11,6 +11,16 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class RegionsService extends BaseService<RegionMaster> {
+  private deathCountSubject = new BehaviorSubject<number>(0);
+  private seriousCountSubject = new BehaviorSubject<number>(0);
+  private slightCountSubject = new BehaviorSubject<number>(0);
+  private propertyDamageCountSubject = new BehaviorSubject<number>(0);
+
+  deathCount$ = this.deathCountSubject.asObservable();
+  seriousCount$ = this.seriousCountSubject.asObservable();
+  slightCount$ = this.slightCountSubject.asObservable();
+  propertyDamageCount$ = this.propertyDamageCountSubject.asObservable();
+
 
   totalVictimEndPoint=`${environment.apiUrl}VictimDetailsTransaction/grouped-region-data?`;
   constructor(protected httpClient: HttpClient) {
@@ -27,4 +37,10 @@ export class RegionsService extends BaseService<RegionMaster> {
     let params = { regionId };
     return this.httpClient.get<any>(this.totalVictimEndPoint, { params: { regionId } });
    }
+   updateCounts(deathCount: number, seriousCount: number, slightCount: number, propertyDamageCount: number) {
+    this.deathCountSubject.next(deathCount);
+    this.seriousCountSubject.next(seriousCount);
+    this.slightCountSubject.next(slightCount);
+    this.propertyDamageCountSubject.next(propertyDamageCount);
+  }
 }
---
+++ src/app/services/trend-analysis/trend-analysis.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SummaryData } from 'src/app/models/get/SummaryData';
+import { environment } from 'src/environments/environment';
+import { BaseService } from '../base-service/BaseService';
+import { RegionBasedSummaryData } from 'src/app/models/get/SummaryCount';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrendAnalysisService extends  BaseService<RegionBasedSummaryData>{
+  summaryData=`${environment.apiUrl}VictimDetailsTransaction/grouped-data`;
+
+  constructor(protected httpClient: HttpClient)  {
+    super(httpClient);
+  }
+  getResourceUrl(): string {
+    return 'VictimDetailsTransaction';
+   }
+   getGroupedData():Observable<any[]>
+   {
+    return this.httpClient.get<any[]>(this.summaryData);
+   }
+}
